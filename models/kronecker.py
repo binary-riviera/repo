ed3205e74193acb655275394819e0015f445a1b9
Daniel Cripps <binary.riviera@gmail.com>
2022-01-15 14:49:17 +0000

    added ability to kronecker snap from python

--- models/kronecker.py
+++ models/kronecker.py
@@ -1,6 +1,27 @@
+import subprocess
+
+
+# TODO: move this to a config file
+snap_dir = "./snap/snap/examples/kronem/"
+command_name = "kronem"
+command = snap_dir + command_name
+
+
 def train():
-    pass
+    subprocess.call(
+        [
+            command,
+            "-i:./data/as20graph.txt",
+            "-n0:2",
+            '-m:"0.9 0.6; 0.6 0.1"',
+            "-ei:50",
+        ],
+    )
 
 
 def kronecker():
     pass
+
+
+if __name__ == "__main__":
+    train()
