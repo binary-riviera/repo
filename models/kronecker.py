3bddbf9bcd08cfc3427ea308fb86ad874e23ab8b
Daniel Cripps <binary.riviera@gmail.com>
2022-01-15 15:17:11 +0000

    added ability to redirect stdout to a log file for kronecker

--- models/kronecker.py
+++ models/kronecker.py
@@ -7,16 +7,30 @@ command_name = "kronem"
 command = snap_dir + command_name
 
 
-def train():
-    subprocess.call(
-        [
-            command,
-            "-i:./data/as20graph.txt",
-            "-n0:2",
-            '-m:"0.9 0.6; 0.6 0.1"',
-            "-ei:50",
-        ],
-    )
+def train(stdout_file=None):
+
+    if stdout_file:
+        with open(stdout_file, "w") as outfile:
+            subprocess.call(
+                [
+                    command,
+                    "-i:./data/as20graph.txt",
+                    "-n0:2",
+                    '-m:"0.9 0.6; 0.6 0.1"',
+                    "-ei:50",
+                ],
+                stdout=outfile,
+            )
+    else:
+        subprocess.call(
+            [
+                command,
+                "-i:./data/as20graph.txt",
+                "-n0:2",
+                '-m:"0.9 0.6; 0.6 0.1"',
+                "-ei:50",
+            ],
+        )
 
 
 def kronecker():
@@ -24,4 +38,4 @@ def kronecker():
 
 
 if __name__ == "__main__":
-    train()
+    train(stdout_file="kronecker.log")
